6478be44283f14cf984872f8c5cca964105df53d
Hiroyuki Tachibana <hiroism007@gmail.com>
2022-09-12 18:00:43 +0900

    wip: it worked

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use halo2_proofs::circuit::Value;
 use halo2_proofs::{
     arithmetic::FieldExt,
     circuit::{Cell, Chip, Layouter, SimpleFloorPlanner},
-    plonk::{Advice, Assigned, Circuit as PlonkCircuit, Column, ConstraintSystem, Error, Fixed, Instance},
+    plonk::{
+        Advice, Assigned, Circuit, Column, ConstraintSystem, Error, Fixed, Instance,
+    },
     poly::Rotation,
 };
-use std::marker::PhantomData;
-use halo2_proofs::plonk::FloorPlanner;
 use pairing::group::ff::Field;
+use std::marker::PhantomData;
 
 #[allow(non_snake_case, dead_code)]
 #[derive(Debug, Clone)]
@@ -38,104 +39,139 @@ impl<F: FieldExt> TutorialChip<F> {
 }
 
 trait TutorialComposer<F: FieldExt> {
-    fn raw_multiply<FM>(&self, layouter: &mut impl Layouter<F>, f: FM) -> Result<(Cell, Cell, Cell), Error>
-        where FM: FnMut() -> Value<(Assigned<F>, Assigned<F>, Assigned<F>)>;
+    fn raw_multiply<FM>(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        f: FM,
+    ) -> Result<(Cell, Cell, Cell), Error>
+    where
+        FM: FnMut() -> Value<(Assigned<F>, Assigned<F>, Assigned<F>)>;
 
-    fn raw_add<FM>(&self, layouter: &mut impl Layouter<F>, f: FM) -> Result<(Cell, Cell, Cell), Error>
-        where FM: FnMut() -> Value<(Assigned<F>, Assigned<F>, Assigned<F>)>;
+    fn raw_add<FM>(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        f: FM,
+    ) -> Result<(Cell, Cell, Cell), Error>
+    where
+        FM: FnMut() -> Value<(Assigned<F>, Assigned<F>, Assigned<F>)>;
 
     fn copy(&self, layout: &mut impl Layouter<F>, a: Cell, b: Cell) -> Result<(), Error>;
 
-    fn expose_public(&self, layout: &mut impl Layouter<F>, cell: Cell, row: usize) -> Result<(), Error>;
+    fn expose_public(
+        &self,
+        layout: &mut impl Layouter<F>,
+        cell: Cell,
+        row: usize,
+    ) -> Result<(), Error>;
 }
 
 impl<F: FieldExt> TutorialComposer<F> for TutorialChip<F> {
-    fn raw_multiply<FM>(&self, layouter: &mut impl Layouter<F>, f: FM) -> Result<(Cell, Cell, Cell), Error> where FM: FnMut() -> Value<(Assigned<F>, Assigned<F>, Assigned<F>)> {
-        layouter.assign_region(|| "mul", |mut region| {
-            let mut values = None;
+    fn raw_multiply<FM>(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        mut f: FM,
+    ) -> Result<(Cell, Cell, Cell), Error>
+    where
+        FM: FnMut() -> Value<(Assigned<F>, Assigned<F>, Assigned<F>)>,
+    {
+        layouter.assign_region(
+            || "mul",
+            |mut region| {
+                let mut values = None;
 
-            let lhs = region.assign_advice(
-                || "lhs",
-                self.config.l,
-                0,
-                || {
-                    values = Some(f());
-                    values.unwrap().map(|v| v.0)
-                },
-            )?;
+                let lhs = region.assign_advice(
+                    || "lhs",
+                    self.config.l,
+                    0,
+                    || {
+                        values = Some(f());
+                        values.unwrap().map(|v| v.0)
+                    },
+                )?;
 
-            let rhs = region.assign_advice(
-                || "rhs",
-                self.config.r,
-                0,
-                || {
-                    values.unwrap().map(|v| v.1)
-                },
-            )?;
+                let rhs = region.assign_advice(
+                    || "rhs",
+                    self.config.r,
+                    0,
+                    || values.unwrap().map(|v| v.1),
+                )?;
 
-            let out = region.assign_advice(
-                || "out",
-                self.config.o,
-                0,
-                || {
-                    values.unwrap().map(|v| v.2)
-                },
-            )?;
+                let out = region.assign_advice(
+                    || "out",
+                    self.config.o,
+                    0,
+                    || values.unwrap().map(|v| v.2),
+                )?;
 
-            region.assign_fixed(|| "m", self.config.sm, 0, || Value::known(F::one()))?;
-            region.assign_fixed(|| "o", self.config.sm, 0, || Value::known(F::one()))?;
+                region.assign_fixed(|| "m", self.config.sm, 0, || Value::known(F::one()))?;
+                region.assign_fixed(|| "o", self.config.sm, 0, || Value::known(F::one()))?;
 
-            Ok((lhs.cell(), rhs.cell(), out.cell()))
-        },)
+                Ok((lhs.cell(), rhs.cell(), out.cell()))
+            },
+        )
     }
 
-    fn raw_add<FM>(&self, layouter: &mut impl Layouter<F>, f: FM) -> Result<(Cell, Cell, Cell), Error> where FM: FnMut() -> Value<(Assigned<F>, Assigned<F>, Assigned<F>)> {
-        layouter.assign_region(|| "add", |mut region| {
-            let mut values = None;
+    fn raw_add<FM>(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        mut f: FM,
+    ) -> Result<(Cell, Cell, Cell), Error>
+    where
+        FM: FnMut() -> Value<(Assigned<F>, Assigned<F>, Assigned<F>)>,
+    {
+        layouter.assign_region(
+            || "add",
+            |mut region| {
+                let mut values = None;
 
-            let lhs = region.assign_advice(
-                || "lhs",
-                self.config.l,
-                0,
-                || {
-                    values = Some(f());
-                    values.unwrap().map(|v| v.0)
-                },
-            )?;
+                let lhs = region.assign_advice(
+                    || "lhs",
+                    self.config.l,
+                    0,
+                    || {
+                        values = Some(f());
+                        values.unwrap().map(|v| v.0)
+                    },
+                )?;
 
-            let rhs = region.assign_advice(
-                || "rhs",
-                self.config.r,
-                0,
-                || {
-                    values.unwrap().map(|v| v.1)
-                },
-            )?;
+                let rhs = region.assign_advice(
+                    || "rhs",
+                    self.config.r,
+                    0,
+                    || values.unwrap().map(|v| v.1),
+                )?;
 
-            let out = region.assign_advice(
-                || "out",
-                self.config.o,
-                0,
-                || {
-                    values.unwrap().map(|v| v.2)
-                },
-            )?;
+                let out = region.assign_advice(
+                    || "out",
+                    self.config.o,
+                    0,
+                    || values.unwrap().map(|v| v.2),
+                )?;
 
-            region.assign_fixed(|| "m", self.config.sm, 0, || Value::known(F::one()))?;
-            region.assign_fixed(|| "o", self.config.sm, 0, || Value::known(F::one()))?;
+                region.assign_fixed(|| "m", self.config.sm, 0, || Value::known(F::one()))?;
+                region.assign_fixed(|| "o", self.config.sm, 0, || Value::known(F::one()))?;
 
-            Ok((lhs.cell(), rhs.cell(), out.cell()))
-        },)
+                Ok((lhs.cell(), rhs.cell(), out.cell()))
+            },
+        )
     }
 
     fn copy(&self, layout: &mut impl Layouter<F>, left: Cell, right: Cell) -> Result<(), Error> {
-        layout.assign_region(|| "copy", |mut region| {
-            region.constrain_equal(left, right)?;
-            region.constrain_equal(left, right)
-        },)
+        layout.assign_region(
+            || "copy",
+            |mut region| {
+                region.constrain_equal(left, right)?;
+                region.constrain_equal(left, right)
+            },
+        )
     }
 
-    fn expose_public(&self, layout: &mut impl Layouter<F>, cell: Cell, row: usize) -> Result<(), Error> {
+    fn expose_public(
+        &self,
+        layout: &mut impl Layouter<F>,
+        cell: Cell,
+        row: usize,
+    ) -> Result<(), Error> {
         layout.constrain_instance(cell, self.config.PI, row)
     }
 }
@@ -144,26 +180,14 @@ impl<F: FieldExt> TutorialComposer<F> for TutorialChip<F> {
 struct TutorialCircuit<F: FieldExt> {
     x: Value<F>,
     y: Value<F>,
-    constant: F
+    constant: F,
 }
 
-pub trait Circuit <F: Field> {
-    type Config: Clone;
-
-    type FloorPlanner: FloorPlanner;
-
-    fn without_witness(&self) -> Self;
-
-    fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config;
-
-    fn synthesize(&self, config: Self::Config, layouter: impl Layouter<F>) -> Result<(), Error>;
-}
-
-impl <F: FieldExt> Circuit<F> for TutorialCircuit<F> {
+impl<F: FieldExt> Circuit<F> for TutorialCircuit<F> {
     type Config = TutorialConfig;
     type FloorPlanner = SimpleFloorPlanner;
 
-    fn without_witness(&self) -> Self {
+    fn without_witnesses(&self) -> Self {
         Self::default()
     }
 
@@ -195,9 +219,11 @@ impl <F: FieldExt> Circuit<F> for TutorialCircuit<F> {
             let sl = meta.query_fixed(sl, Rotation::cur());
             let sr = meta.query_fixed(sr, Rotation::cur());
             let so = meta.query_fixed(so, Rotation::cur());
+            let sm = meta.query_fixed(sm, Rotation::cur());
             let sc = meta.query_fixed(sc, Rotation::cur());
 
-            vec![l.clone() * sl + r.clone() * sr + l * r * sm + (o * so (-F::one())) + sc]
+            vec![l.clone() * sl + r.clone() * sr + l * r * sm + (o * so * (-F::one())) + sc]
+
         });
 
         meta.create_gate("public input", |meta| {
@@ -205,7 +231,7 @@ impl <F: FieldExt> Circuit<F> for TutorialCircuit<F> {
             #[allow(non_snake_case)]
             let PI = meta.query_instance(PI, Rotation::cur());
             let sp = meta.query_fixed(sp, Rotation::cur());
-            vec![sp * (l -PI)]
+            vec![sp * (l - PI)]
         });
 
         TutorialConfig {
@@ -217,26 +243,77 @@ impl <F: FieldExt> Circuit<F> for TutorialCircuit<F> {
             so,
             sm,
             sc,
-            PI
+            PI,
         }
-
     }
 
-    fn synthesize(&self, config: Self::Config, mut layouter: impl Layouter<F>) -> Result<(), Error> {
+    fn synthesize(
+        &self,
+        config: Self::Config,
+        mut layouter: impl Layouter<F>,
+    ) -> Result<(), Error> {
         let cs = TutorialChip::new(config);
 
         let x: Value<Assigned<_>> = self.x.into();
         let y: Value<Assigned<_>> = self.y.into();
         let consty = Assigned::from(self.constant);
-        
-        let (a0, b0, c0) = cs.raw_multiply(&mut layouter, || x.map(|x|(x, x, x**x)))?;
 
-        let (a1, b1, c1) = cs.raw_multiply(&mut layouter, || y.map(|y|(y, y, y**y)))?;
+        let (a0, b0, c0) = cs.raw_multiply(&mut layouter, || x.map(|x| (x, x, x * x)))?;
+        // a0 と b0 は同じ値
+        cs.copy(&mut layouter, a0, b0)?;
 
+        let (a1, b1, c1) = cs.raw_multiply(&mut layouter, || y.map(|y| (y, y, y * y)))?;
+        // a1 と b1 は同じ値
+        cs.copy(&mut layouter, a1, b1)?;
 
+        // Create xy squared
+        let (a2, b2, c2) = cs.raw_multiply(&mut layouter, || {
+            x.zip(y).map(|(x, y)| (x * x, y * y, x * x * y * y))
+        })?;
+        // c0 と a2 は同じ値
+        cs.copy(&mut layouter, c0, a2)?;
+        // c1 と b2 は同じ値
+        cs.copy(&mut layouter, c1, b2)?;
+
+        // Add the constant
+        let (a3, b3, c3) = cs.raw_add(&mut layouter, || {
+            x.zip(y)
+                .map(|(x, y)| (x * x * y * y, consty, x * x * y * y + consty))
+        })?;
+        //c2 と a3 は同じ値
+        cs.copy(&mut layouter, c2, a3)?;
+
+        // Ensure that the constant in the TutorialCircuit struct is correctly used and that the
+        // result of the circuit computation is what is expected. (use expose_public))
+        cs.expose_public(&mut layouter, b3, 0)?;
+        // Below is another way to expose a public value, this time the output value of the computation
+        // (Use constrain_instance)
+        layouter.constrain_instance(c3, cs.config.PI, 1)?;
+
+        Ok(())
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    use halo2_proofs::dev::MockProver;
+    use halo2_proofs::halo2curves::bn256::Fr as Fp;
+
+    let k = 4;
+    let constant = Fp::from(7);
+
+    let x = Fp::from(5);
+    let y = Fp::from(9);
+
+    let z = Fp::from(25 * 81 + 7);
+
+    let circuit = TutorialCircuit {
+        x: Value::known(x),
+        y: Value::known(y),
+        constant,
+    };
+    let mut public_inputs = vec![constant, z];
+
+    let prover = MockProver::run(k, &circuit, vec![public_inputs.clone()]).unwrap();
+
+    assert_eq!(prover.verify(), Ok(()));
 }
